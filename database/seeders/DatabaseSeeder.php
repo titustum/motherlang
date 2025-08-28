fc9c632709373b4bc3e75aa97ef568108ec2e331
titustum <tituskiptanuitum@gmail.com>
2025-08-28 17:14:36 +0300

    Improved seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\EnglishConcept;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,8 +17,16 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin User',
+            'email' => 'admin@mail.com',
+            'role' => 'admin'
+        ]);
+
+        $this->call([
+            ConceptCategorySeeder::class,
+            EnglishConceptSeeder::class,
+            LanguageGroupSeeder::class,
+            LanguageSeeder::class,
         ]);
     }
 }
